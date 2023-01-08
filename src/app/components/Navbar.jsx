74bf3ec634e873be8c49c2abf94ce8407fc5601c
Jorge Gomez <jorge.luis-leones@hotmail.com>
2023-01-07 23:34:05 -0500

    update

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styles.css'
 
 const Navbar = () => {
 
@@ -16,32 +17,71 @@ const Navbar = () => {
                         <span className='image'>
                             <img src={'https://i.pinimg.com/736x/1f/04/9a/1f049ada9040b9311078aa28110badde.jpg'} alt="Logo" />
                         </span>
+
+                        <div className='text header-text'>
+                            <span className='name'>Name Inventario</span>
+                        </div>
                     </div>
 
-                    <div className='text header-text'>
-                        <span className='name'>Name Inventario</span>
+                    <i class='bx bx-chevron-right toggle'></i>
+
+                    <div className='menu-bar'>
+                        <div className='menu'>
+
+                            <li className='search-box'>
+                                <i class='bx bx-search icon'></i>
+                                <input type="search" placeholder='Search ...' />
+                            </li>
+
+
+                            <ul className='menu-links'>
+
+                                <li className='nav-link'>
+                                    <Link to='/home'>
+                                        <i class='bx bx-home-alt icon'></i>
+                                        <span className='text nav-text'>Home</span>
+                                    </Link>
+                                </li>
+
+                                <li className='nav-link'>
+                                    <Link to='/Dashboard'>
+                                        <i class='bx bxs-dashboard icon' ></i>
+                                        <span className='text nav-text'>Dashboard</span>
+                                    </Link>
+                                </li>
+
+                                <li className='nav-link'>
+                                    <Link to='/Users'>
+                                        <i class='bx bxs-user-detail icon'></i>
+                                        <span className='text nav-text'>Users</span>
+                                    </Link>
+                                </li>
+
+                                <li className='nav-link'>
+                                    <Link to='/Inventory'>
+                                    <i class='bx bxs-store icon'></i>
+                                        <span className='text nav-text'>Inventory</span>
+                                    </Link>
+                                </li>
+
+                                <li className='nav-link'>
+                                    <Link to='/Admin'>
+                                    <i class='bx bxs-user-circle icon'></i>
+                                        <span className='text nav-text'>Admin</span>
+                                    </Link>
+                                </li>
+
+                                <li className='nav-link'>
+                                    <Link to='/'>
+                                    <i class='bx bx-log-out icon'></i>
+                                        <span className='text nav-text'>Logout</span>
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
                     </div>
 
-                    <ul>
-                        <li>
-                            <Link to='/home'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/dashboard'>Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link to='/users'>Users</Link>
-                        </li>
-                        <li>
-                            <Link to='/inventory'>Inventory</Link>
-                        </li>
-                        <li>
-                            <Link to='/admin'>Admin</Link>
-                        </li>
-                        <li>
-                            <Link to='/'>Logout</Link>
-                        </li>
-                    </ul>
                 </header>
             </nav>
         </div>
